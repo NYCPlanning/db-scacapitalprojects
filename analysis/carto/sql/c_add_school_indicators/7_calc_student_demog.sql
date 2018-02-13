83fbc14b616126b53f544c57d662a977149e50b6
mqli322 <30453673+mqli322@users.noreply.github.com>
2018-02-12 20:27:51 -0500

    Update 7_calc_student_demog.sql

--- analysis/carto/sql/c_add_school_indicators/7_calc_student_demog.sql
+++ analysis/carto/sql/c_add_school_indicators/7_calc_student_demog.sql
@@ -11,3 +11,16 @@ WHERE x_citywide is null
 AND x_alternative is null
 AND x_school_type = 'General Academic'
 GROUP BY geo_csd ASC
+
+-- Calc school quality review score (average of schools in area with score)
+
+SELECT
+	geo_csd,
+    sum(blended_sqr_score)/ count(blended_sqr_score) AS sqr,
+    count(blended_sqr_score) AS num_w_sqr
+FROM capitalplanning.doe_2016_demographics
+WHERE x_citywide is null
+AND x_alternative is null
+AND x_school_type = 'General Academic'
+AND blended_sqr_score is not null
+GROUP BY geo_csd
