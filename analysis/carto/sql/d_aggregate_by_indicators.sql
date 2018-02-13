3d2135ee1cdf274aa0ff4c65e4f00964bd07d470
mqli322 <30453673+mqli322@users.noreply.github.com>
2018-02-12 19:23:06 -0500

    Create d_aggregate_by_indicators.sql

---
+++ analysis/carto/sql/d_aggregate_by_indicators.sql
@@ -0,0 +1,18 @@
+--Identify high charter areas 
+(charter enrollment/total enrollment by school district, excl. enrollment from alternative & citywide programs)
+-- Source data: doe_2016_demographics
+
+SELECT
+	geo_csd,
+  x_charter,
+  sum(total_enrollment) AS total_enroll
+FROM capitalplanning.doe_2016_demographics
+WHERE x_alternative is null
+AND x_citywide is null
+GROUP BY 
+	geo_csd,
+  x_charter
+  
+  
+--Identify overutilized areas
+(as recorded in BB, excl. alternative & citywide. includes SPED)
