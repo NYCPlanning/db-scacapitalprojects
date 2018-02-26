dcccd830e0bcf0fe30b6f865776724ba868437bb
mqli322 <30453673+mqli322@users.noreply.github.com>
2018-02-26 15:31:19 -0500

    Update calc_per_student.sql

--- analysis/carto/sql/calc_per_student.sql
+++ analysis/carto/sql/calc_per_student.sql
@@ -11,14 +11,11 @@ GROUP BY geo_dist
 -- Exclude citywide and alternative HS (include co-located charter and SPED)
 
 SELECT 
-	geo_dist,
-  	geo_subdist,
-    	sum(ps_enroll) AS ps_enroll,
-   	 sum(ps_capacity) AS ps_cap,
-  	sum(ms_enroll) AS ms_enroll,
-    	sum(ms_capacity) AS ms_cap
+	geo_borocd,
+    	sum(org_enroll) AS org_enroll,
+   	sum(org_target_cap) AS org_cap
 FROM capitalplanning.doe_bb_201617_for_analysis
 WHERE x_citywide is null
 AND x_alternative is null
-GROUP BY geo_dist, geo_subdist
-ORDER BY geo_dist, geo_subdist
+GROUP BY geo_borocd
+ORDER BY geo_borocd
