350c255812ee6037bfc6ceef021f3304e2894d24
Amoli Vani <amolivani@gmail.com>
2018-06-12 16:54:48 -0400

    Adding data loading scripts and sca capacity projects

---
+++ scacapitalprojects_build/01_dataloading.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+################################################################################################
+### OBTAINING DATA
+################################################################################################
+### NOTE: This script requires you to setup the DATABASE_URL environment variable.
+### Directions are in the README.md.
+
+## Load all datasets from sources using the civic data loader
+## https://github.com/NYCPlanning/data-loading-scripts
+
+cd '/prod/data-loading-scripts'
+
+## Open_datasets - PULLING FROM OPEN DATA
+echo 'Loading open source datasets...'
+node loader.js install sca_cp_added_projects
+node loader.js install sca_cp_advanced_projects
+node loader.js install sca_cancelled_projects
+node loader.js install sca_cp_cap_location
+node loader.js install sca_cp_class_size_reduction
+node loader.js install sca_cp_prek_location
+node loader.js install sca_cp_prek_schools
+node loader.js install sca_cp_programs
+node loader.js install sca_cp_rep_schools
+
+## Other_datasets - PULLING FROM PLUTO GitHub repo
+##echo 'Loading datasets from PLUTO GitHub repo...'
---
+++ scacapitalprojects_build/03_build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# make sure we are at the top of the git directory
+REPOLOC="$(git rev-parse --show-toplevel)"
+cd $REPOLOC
+
+# load config
+DBNAME=$(cat $REPOLOC/sca.config.json | jq -r '.DBNAME')
+DBUSER=$(cat $REPOLOC/sca.config.json | jq -r '.DBUSER')
+
+start=$(date +'%T')
+echo "Starting to build SCA"
+
+# create the table
+echo 'Creating base SCA table'
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/scacapitalprojects_build/sql/create_sca_cp_projects.sql
+	# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/bbl.sql
+
+	# # populate RPAD data
+	# echo 'Adding on RPAD data attributes'
+	# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/allocated.sql
+	# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/geocodes.sql
+
+	# # add on CAMA data attributes
+	# echo 'Adding on CAMA data attributes'
+	# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/cama_bsmttype.sql
+	# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/cama_lottype.sql
+	# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/cama_proxcode.sql
+	# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/cama_bldgarea.sql
+	# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/cama_easements.sql
+
+	# # populate other fields from misc sources
+	# echo 'Adding on data attributes from other sources'
+	# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/landuse.sql
+	# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/lpc.sql
+	# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/zoning.sql
+	# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/far.sql
+	# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/edesignation.sql
+
+	# echo 'Transform RPAD data attributes'
+	# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/irrlotcode.sql
+	# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/colp.sql
+
+	# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/ipis.sql
