ec7f8f847456b4004981c6f33dfc29973a4f66a2
Amoli Vani <amolivani@gmail.com>
2018-06-13 16:58:21 -0400

    Export file

---
+++ scacapitalprojects_build/04_export.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# make sure we are at the top of the git directory
+REPOLOC="$(git rev-parse --show-toplevel)"
+cd $REPOLOC
+
+# load config
+DBNAME=$(cat $REPOLOC/sca.config.json | jq -r '.DBNAME')
+DBUSER=$(cat $REPOLOC/sca.config.json | jq -r '.DBUSER')
+
+# eventually these should copy directly from psql to carto
+# for now, write to files which can by copied
+
+# points
+ogr2ogr -u $DBUSER -f scacapitalprojects_build/output/sca_cp_capacity_projects $DBNAME "SELECT * FROM sca_cp_capacity_projects AND ST_GeometryType(geom)='ST_Point'"
+ogr2ogr -u $DBUSER -f scacapitalprojects_build/output/sca_cp_school_programs $DBNAME "SELECT * FROM sca_cp_school_programs AND ST_GeometryType(geom)='ST_Point'"
+ogr2ogr -u $DBUSER -f scacapitalprojects_build/output/sca_cp_projects $DBNAME "SELECT * FROM sca_cp_projects AND ST_GeometryType(geom)='ST_Point'"
+
+# Output individual helper tables
+# cpdb_commitments
+psql -U $DBUSER -d $DBNAME -c "COPY( SELECT * FROM sca_cp_capacity_projects) TO '$REPOLOC/scacapitalprojects_build/output/sca_cp_capacity_projects.csv' DELIMITER ',' CSV HEADER;"
+psql -U $DBUSER -d $DBNAME -c "COPY( SELECT * FROM sca_cp_school_programs) TO '$REPOLOC/scacapitalprojects_build/output/sca_cp_school_programs.csv' DELIMITER ',' CSV HEADER;"
+psql -U $DBUSER -d $DBNAME -c "COPY( SELECT * FROM sca_cp_projects) TO '$REPOLOC/scacapitalprojects_build/output/sca_cp_projects.csv' DELIMITER ',' CSV HEADER;"
