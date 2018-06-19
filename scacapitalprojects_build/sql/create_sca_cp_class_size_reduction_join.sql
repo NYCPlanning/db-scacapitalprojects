03f4703fc80d39ad17a05d633cb3b276c51ad922
Amoli Vani <amolivani@gmail.com>
2018-06-19 11:06:15 -0400

    Adding extra fields in tables

--- scacapitalprojects_build/sql/create_sca_cp_class_size_reduction_join.sql
+++ scacapitalprojects_build/sql/create_sca_cp_class_size_reduction_join.sql
@@ -2,14 +2,14 @@
 DROP TABLE IF EXISTS sca_cp_class_size_reduction_join;
 CREATE TABLE sca_cp_class_size_reduction_join AS 
 (
-SELECT district, school, projectnum, totalestcost, substring(actualestcompl, 1, strpos (actualestcompl,'-')-1) actualestcompl_year,
-substring(actualestcompl, strpos(actualestcompl,'-')+1, length(actualestcompl)) actualestcompl_month
+SELECT existingsiteidentified, district, school, projectnum, forecastcapacity, designstart, constrstart, actualestcompl,totalestcost, substring(actualestcompl, 1, strpos (actualestcompl,'-')-1) actualestcompl_year,
+substring(actualestcompl, strpos(actualestcompl,'-')+1, length(actualestcompl)) actualestcompl_month, previousappropriations, fundingreqdfy1519, neededtocomplete
 FROM sca_cp_class_size_reduction
 );
 
-
-
 ALTER TABLE sca_cp_class_size_reduction_join ADD fy INT;
+
+ALTER TABLE sca_cp_class_size_reduction_join ADD proposedleasedfacility text;
 
 ALTER TABLE sca_cp_class_size_reduction_join ALTER COLUMN actualestcompl_year TYPE INTEGER USING (trim(actualestcompl_year)::integer);
 
@@ -18,7 +18,6 @@ SET fy = CASE
     WHEN (actualestcompl_month IN ('Jul','Aug', 'Sep', 'Oct', 'Nov', 'Dec') ) THEN actualestcompl_year+2001
     ELSE actualestcompl_year+2000
     END ;
-
 
 ALTER TABLE sca_cp_class_size_reduction_join ADD longitude text;
 ALTER TABLE sca_cp_class_size_reduction_join ADD latitude text;
