619bec625e19548744c272c61446781ba63748a9
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-12-03 13:43:20 -0500

    correcting output scripts

--- scacapitalprojects_build/03_export.sh
+++ scacapitalprojects_build/03_export.sh
@@ -12,9 +12,10 @@ DBUSER=$(cat $REPOLOC/sca.config.json | jq -r '.DBUSER')
 # for now, write to files which can by copied
 
 # points
-ogr2ogr -u $DBUSER -f scacapitalprojects_build/output/sca_cp_capacity_projects $DBNAME "SELECT * FROM sca_cp_capacity_projects AND ST_GeometryType(geom)='ST_Point'"
-ogr2ogr -u $DBUSER -f scacapitalprojects_build/output/sca_cp_school_programs $DBNAME "SELECT * FROM sca_cp_school_programs AND ST_GeometryType(geom)='ST_Point'"
-ogr2ogr -u $DBUSER -f scacapitalprojects_build/output/sca_cp_projects $DBNAME "SELECT * FROM sca_cp_projects AND ST_GeometryType(geom)='ST_Point'"
+ogr2ogr -f "GeoJSON" scacapitalprojects_build/output/sca_cp_capacity_projects.geojson PG:"host=localhost dbname=$DBNAME user=$DBUSER" -sql "SELECT * FROM sca_cp_capacity_projects WHERE ST_GeometryType(geom)='ST_Point'"
+ogr2ogr -f "GeoJSON" scacapitalprojects_build/output/sca_cp_school_programs.geojson PG:"host=localhost dbname=$DBNAME user=$DBUSER" -sql "SELECT * FROM sca_cp_school_programs WHERE ST_GeometryType(geom)='ST_Point'"
+#ogr2ogr -f "GeoJSON" scacapitalprojects_build/output/sca_cp_projects.geojson PG:"host=localhost dbname=$DBNAME user=$DBUSER" \
+#-sql "SELECT * FROM sca_cp_projects AND ST_GeometryType(geom)='ST_Point'"
 
 # Output individual helper tables
 # cpdb_commitments
@@ -25,6 +26,6 @@ psql -U $DBUSER -d $DBNAME -c "COPY( SELECT * FROM qa_summary_stats) TO '$REPOLO
 
 \copy (SELECT * FROM sca_cp_capacity_projects) TO '/prod/db-scacaptialprojects/scacapitalprojects_build/output/sca_cp_capacity_projects.csv' DELIMITER ',' CSV HEADER;
 \copy (SELECT * FROM sca_cp_school_programs) TO '/prod/db-scacaptialprojects/scacapitalprojects_build/output/sca_cp_school_programs.csv' DELIMITER ',' CSV HEADER;
-\copy (SELECT * FROM sca_cp_projects) TO '/prod/db-scacaptialprojects/scacapitalprojects_build/output/sca_cp_projects.csv' DELIMITER ',' CSV HEADER;
+#\copy (SELECT * FROM sca_cp_projects) TO '/prod/db-scacaptialprojects/scacapitalprojects_build/output/sca_cp_projects.csv' DELIMITER ',' CSV HEADER;
 \copy (SELECT * FROM qa_summary_stats) TO '/prod/db-scacaptialprojects/scacapitalprojects_build/output/qa_summary_stats.csv'DELIMITER ',' CSV HEADER;
 \copy (SELECT * FROM sca_output_carto) TO '/prod/db-scacaptialprojects/scacapitalprojects_build/output/sca_output_carto.csv'DELIMITER ',' CSV HEADER;
